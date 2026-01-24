1075b31e51392cde483e61bd89b60aa5d72b7b8b
AlejandroMtz07 <alejandro23juanito@gmail.com>
2026-01-24 15:28:11 -0700

    Adding animation dependency

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,12 +1,15 @@
-import { Link } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 
 export default function HomeView() {
-  return (
-    <div>
-        <NavBar/>
-        
-    </div>
-  )
+    return (
+        <div className="bg-white/100 min-h-screen">
+            <NavBar />
+            <main className="grid grid-cols-2">
+                <div className="bg-black p-4">
+
+                </div>
+            </main>
+        </div>
+    )
 }
