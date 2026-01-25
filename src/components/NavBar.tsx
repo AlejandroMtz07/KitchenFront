33e47f19181cd7145cbbb19c00b4b9ec12e2f94c
AlejandroMtz07 <alejandro23juanito@gmail.com>
2026-01-24 18:53:02 -0700

    Adding images and changing styles in homepage and navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,14 +6,14 @@ export default function NavBar() {
 
         <nav
             className="lg:p-20 p-7 bg-transparent items-center flex 
-                    justify-between gap-1 lg:gap-10 border-b-2 border-b-gray-200"
+                    justify-between gap-1 lg:gap-10 border-b-2 border-b-gray-200 sticky top-0"
         >
             <div className="flex items-center lg:gap-4 gap-2">
                 <Link to={'/'}>
                     <img src="/kitchen-logo.svg" height={100} width={100} />
                 </Link>
                 <h1 className="uppercase font-extralight lg:text-3xl 
-                    text-lg lg:motion-preset-typewriter-[15]"
+                    text-lg lg:motion-preset-typewriter-[15] lg:motion-duration-[8s]"
                 >
                     Kitchen Recipes
                 </h1>
--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -3,11 +3,32 @@ import NavBar from "../components/NavBar";
 
 export default function HomeView() {
     return (
-        <div className="bg-white/100 min-h-screen">
+        <div className="bg-white min-h-screen">
             <NavBar />
-            <main className="grid grid-cols-2">
-                <div className="bg-black p-4">
-
+            <main className="lg:grid lg:grid-cols-2 p-4 flex flex-col">
+                <div className="p-4 mt-40 m-20 border-gray-200 flex items-center rounded-lg
+                    hover:shadow-xl border-2 transition text-center leading-loose font-thin">
+                    On this website you will be able to upload your favorite recipes, special recipes, 
+                    which can be private if you don't want others to copy your special 
+                    touch, or public to share your great cooking skills.
+                </div>
+                <div className="p-4">
+                    <img 
+                        src="/images/breakfast.jpg" 
+                        alt="Breakfast image" 
+                        className="rounded-lg opacity-75"/>
+                </div>
+                <div className="p-4">
+                    <img 
+                        src="/images/recipe-book.jpg" 
+                        alt="Recipe book" 
+                        className="rounded-lg opacity-75 relative"
+                    />
+                </div>
+                <div className="p-4 mt-40 m-20 border-gray-200 flex items-center rounded-lg
+                    hover:shadow-xl border-2 transition text-center leading-loose font-thin">
+                    Also you  will be able to save public recipes to your private recipe book from 
+                    other users if you want to save for later his recipe.
                 </div>
             </main>
         </div>
