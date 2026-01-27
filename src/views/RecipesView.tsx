0d33f041a14b16c652f4d1f6dc549e653de3e46f
AlejandroMtz07 <alejandro23juanito@gmail.com>
2026-01-27 15:21:59 -0700

    Initial steps of the recipe searcher.

--- src/views/RecipesView.tsx
+++ src/views/RecipesView.tsx
@@ -1,7 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import { getRecipes } from "../api/RecipesApi";
 import { Navigate } from "react-router-dom";
-import {ArrowPathIcon} from '@heroicons/react/24/outline';
+import {ArrowPathIcon, MagnifyingGlassIcon} from '@heroicons/react/24/outline';
+import { useMemo, useState } from "react";
 
 export default function RecipesView() {
     const { data, isLoading, isError } = useQuery({
@@ -9,6 +10,15 @@ export default function RecipesView() {
         queryKey: ['recipes'],
         retry: 1
     });
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const handleInputsearch = useMemo(()=>{
+        return data?.filter(recipe => 
+            recipe.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            recipe.user_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            recipe.ingredients.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+    },[data,searchTerm]);
 
     if (isError) {
         return <Navigate to={'/auth/login'} />
@@ -26,10 +36,22 @@ export default function RecipesView() {
             <h1 className="text-center p-5 font-extralight text-2xl uppercase">
                 !Your amazing recipe book¡
             </h1>
+                <div className="flex flex-row lg:m-10 items-center">
+                    <label htmlFor="searchTerm">
+                        <MagnifyingGlassIcon width={20}/>
+                    </label>
+                    <input 
+                        type="text" id="searchTerm" 
+                        placeholder="Search by recipe name, author or ingredients" 
+                        className="ml-5 lg:w-96 text-xs w-40 bg-transparent bg-gray-200 p-4 rounded"
+                        onChange={(e)=>setSearchTerm(e.target.value)}
+                    />
+                    <button>Add recipe</button>
+                </div>
             <div className="lg:grid lg:grid-cols-2 lg:p-20 p-10 lg:gap-10 gap-5
                 flex flex-col text-center"
             >
-                {data.map(recipe => (
+                {handleInputsearch && handleInputsearch.map(recipe => (
                     <div
                         className="bg-white border-2 border-gray-300 
                         rounded grid grid-cols-2 items-center p-10 hover:shadow-lg transition"
