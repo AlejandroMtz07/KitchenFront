1e062e6d597827a4d163fcf12c4a3162a33ae626
AlejandroMtz07 <alejandro23juanito@gmail.com>
2026-01-30 12:31:38 -0700

    Adding some information to the home page.

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -8,27 +8,39 @@ export default function HomeView() {
             <main className="lg:grid lg:grid-cols-2 p-4 flex flex-col">
                 <div className="p-4 mt-40 m-20 border-gray-200 flex items-center rounded-lg
                     hover:shadow-xl border-2 transition text-center leading-loose font-thin">
-                    On this website you will be able to upload your favorite recipes, special recipes, 
-                    which can be private if you don't want others to copy your special 
+                    On this website you will be able to upload your favorite recipes, special recipes,
+                    which can be private if you don't want others to copy your special
                     touch, or public to share your great cooking skills.
                 </div>
                 <div className="p-4">
-                    <img 
-                        src="/images/breakfast.jpg" 
-                        alt="Breakfast image" 
-                        className="rounded-lg opacity-75"/>
+                    <img
+                        src="/images/breakfast.jpg"
+                        alt="Breakfast image"
+                        className="rounded-lg opacity-75" />
                 </div>
                 <div className="p-4">
-                    <img 
-                        src="/images/recipe-book.jpg" 
-                        alt="Recipe book" 
+                    <img
+                        src="/images/recipe-book.jpg"
+                        alt="Recipe book"
                         className="rounded-lg opacity-75"
                     />
                 </div>
                 <div className="p-4 mt-40 m-20 border-gray-200 flex items-center rounded-lg
                     hover:shadow-xl border-2 transition text-center leading-loose font-thin">
-                    Also you  will be able to save public recipes to your private recipe book from 
+                    Also you  will be able to save public recipes to your private recipe book from
                     other users if you want to save for later his recipe.
+                </div>
+                <div className="p-4 mt-40 m-20 border-gray-200 flex items-center rounded-lg
+                    hover:shadow-xl border-2 transition text-center leading-loose font-thin">
+                    Finally, you will be able to see the public recipes from the user that you want, 
+                    like famous chefs or people you know that use this app.
+                </div>
+                <div>
+                    <img 
+                        src="/images/profile.png" 
+                        alt="Profile photo" 
+                        className="rounded-lg opacity-75"
+                    />
                 </div>
             </main>
         </div>
