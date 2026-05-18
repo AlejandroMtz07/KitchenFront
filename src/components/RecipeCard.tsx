18783633bdbb6a4619f94147e3c3ee222235c9db
AlejandroMtz07 <alejandro23juanito@gmail.com>
2026-05-18 13:34:45 -0700

    Changing recipe image style.

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -64,10 +64,11 @@ export default function RecipeCard({ recipe }: RecipeCardProps) {
 
     return (
         <>
-            <div className="rounded-3xl lg:rounded-none bg-white-200 border-2 
+            <div className=" lg:rounded-none bg-white-200 border-2 
                 border-b-gray-300 shadow-sm shadow-gray-300 align-middle 
-                flex flex-row items-center justify-between mt-10 lg:mt-0 lg:p-20">
-                <div className="text-center lg:p-10 p-5 lg:text-lg text-sm">
+                flex flex-col items-center justify-between lg:mb-10 mb-5 ">
+                <img src={recipe.image} alt={recipe.description} className="lg:w-full w-32" />
+                <div className="text-center lg:p-5 p-2 lg:text-lg text-sm">
                     {
                         !isEditing ?
                             <>
@@ -139,7 +140,6 @@ export default function RecipeCard({ recipe }: RecipeCardProps) {
                     }
                 </div>
                 <div className="flex flex-col">
-                    <img src={recipe.image} alt={recipe.description} className="lg:w-40 w-32 rounded-3xl mt-2" />
                     <div className="flex lg:justify-end p-2">
                         {username === recipe.user_username ?
                             (!isEditing ?
--- src/views/RecipesView.tsx
+++ src/views/RecipesView.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { getRecipes } from "../api/RecipesApi";
 import { Link, Navigate } from "react-router-dom";
-import { ArrowDownTrayIcon, ArrowPathIcon, FaceFrownIcon, MagnifyingGlassIcon, PlusIcon, XMarkIcon} from '@heroicons/react/24/outline';
+import { ArrowDownTrayIcon, ArrowPathIcon, FaceFrownIcon, MagnifyingGlassIcon, PlusIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import { useMemo, useState } from "react";
 import RecipeCard from "../components/RecipeCard";
 
@@ -13,13 +13,13 @@ export default function RecipesView() {
     });
 
     const [searchTerm, setSearchTerm] = useState('');
-    const handleInputsearch = useMemo(()=>{
-        return data?.filter(recipe => 
+    const handleInputsearch = useMemo(() => {
+        return data?.filter(recipe =>
             recipe.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
             recipe.user_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
             recipe.ingredients.toLowerCase().includes(searchTerm.toLowerCase())
         )
-    },[data,searchTerm]);
+    }, [data, searchTerm]);
 
     if (isError) {
         return <Navigate to={'/auth/login'} />
@@ -27,62 +27,66 @@ export default function RecipesView() {
 
     if (isLoading) return (
         <div className="flex justify-center items-center h-screen animate-spin">
-            <ArrowPathIcon height={40} width={40}/>
+            <ArrowPathIcon height={40} width={40} />
         </div>
     )
 
     return (
-        data?.length!>0 ?
-        <>
-            <h1 className="text-center p-5 mt-5 font-extralight text-2xl uppercase">
-                !Your amazing recipe book¡
-            </h1>
+        data?.length! > 0 ?
+            <>
+                {/* Recipes view title */}
+                <h1 className="text-center p-5 mt-5 font-extralight text-2xl uppercase">
+                    !Your amazing recipe book¡
+                </h1>
+                {/* Container for the search recipe input, add and download button */}
                 <div className="flex lg:flex-row flex-col lg:m-10 lg:ml-20 items-center print:hidden">
+                    {/* Search recipe input */}
                     <label htmlFor="searchTerm" className="flex flex-row">
-                        <MagnifyingGlassIcon width={20}/>
-                        <input 
-                            type="text" id="searchTerm" 
-                            placeholder="Search by recipe name, author or ingredients" 
+                        <MagnifyingGlassIcon width={20} />
+                        <input
+                            type="text" id="searchTerm"
+                            placeholder="Search by recipe name, author or ingredients"
                             className="ml-5 lg:w-96 text-xs w-56 bg-transparent bg-gray-200 p-4 rounded"
                             value={searchTerm}
-                            onChange={(e)=>setSearchTerm(e.target.value)}
+                            onChange={(e) => setSearchTerm(e.target.value)}
                         />
-                        <XMarkIcon width={20} className="ml-5 cursor-pointer" onClick={()=>setSearchTerm('')}/>
+                        <XMarkIcon width={20} className="ml-5 cursor-pointer" onClick={() => setSearchTerm('')} />
                     </label>
                     <div className="flex flex-row gap-2">
+                        {/* Add new recipe button */}
                         <div className="flex flex-row p-2 gap-2 border-b-black border-b-2 
                             lg:ml-10 uppercase text-xs print:hidden">
                             <PlusIcon width={20} />
                             <Link to={'/recipes/new'}>New recipe</Link>
                         </div>
+                        {/* Download the recipe book button */}
                         <div className="flex flex-row p-2 gap-2 border-b-black border-b-2 
                             lg:ml-10 text-xs print:hidden">
-                                <ArrowDownTrayIcon width={20} className="text-green-600" strokeWidth={3}/>
-                            <button onClick={()=>window.print()} className="uppercase">
+                            <ArrowDownTrayIcon width={20} />
+                            <button onClick={() => window.print()} className="uppercase">
                                 Download
                             </button>
                         </div>
                     </div>
                 </div>
-            <div className="lg:p-10 p-5 lg:gap-10 gap-5
-                flex flex-col text-center lg:grid grid-cols-2"
-            >
-                {handleInputsearch && handleInputsearch.map(recipe => (
-                    <RecipeCard recipe={recipe} key={recipe.id}/>
-                ))}
+                {/* List of filtered recipes or just all the recipes */}
+                <div className="lg:m-20 m-5 grid grid-cols-3 gap-5">
+                    {handleInputsearch && handleInputsearch.map(recipe => (
+                        <RecipeCard recipe={recipe} key={recipe.id} />
+                    ))}
+                </div>
+            </> :
+            <div className="flex flex-col items-center justify-center p-10">
+                <div className="flex items-center">
+                    <h1 className="text-center font-extralight text-2xl p-5">
+                        There's any recipes saved
+                    </h1>
+                    <FaceFrownIcon width={20} className="w-6 h-6" />
+                </div>
+                <p className="text-xs uppercase">
+                    Check for more recipes
+                    <Link to={'/recipes'} className="text-blue-700 font-bold"> here</Link>
+                </p>
             </div>
-        </> : 
-        <div className="flex flex-col items-center justify-center p-10">
-            <div className="flex items-center">
-                <h1 className="text-center font-extralight text-2xl p-5">
-                    There's any recipes saved 
-                </h1>
-                <FaceFrownIcon width={20} className="w-6 h-6"/>
-            </div>
-            <p className="text-xs uppercase">
-                Check for more recipes 
-                <Link to={'/recipes'} className="text-blue-700 font-bold"> here</Link>
-            </p>
-        </div>
     )
 }
