e02a5f97ffffe6e0cc5c63b59a3088fd73456461
AlejandroMtz07 <alejandro23juanito@gmail.com>
2026-05-18 23:44:32 -0700

    Making the navbar responsive for mobile devices and small screens.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,86 +1,166 @@
-import { ArrowLeftStartOnRectangleIcon, ArrowRightEndOnRectangleIcon, ArrowTopRightOnSquareIcon, BookmarkIcon, BookOpenIcon } from "@heroicons/react/24/outline";
+import { ArrowLeftStartOnRectangleIcon, ArrowRightEndOnRectangleIcon, ArrowTopRightOnSquareIcon, Bars3Icon, BookmarkIcon, BookOpenIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 
 export default function NavBar() {
 
+    // Variable to get the current user location.
     const location = useLocation();
+    const queryClient = useQueryClient();
 
+    const [isOpen, setIsOpen] = useState(false);
+
+    // List with protected links
     const protectedLinks = [
         '/recipes/book',
         '/recipes/new'
     ]
 
-    const queryClient = useQueryClient();
 
-    const logout = ()=>{
+    // Logout function
+    const logout = () => {
         localStorage.removeItem('token');
-        queryClient.invalidateQueries({queryKey:['recipes']});
+        queryClient.invalidateQueries({ queryKey: ['recipes'] });
     }
 
     return (
 
         <nav
-            className="lg:p-10 p-7 bg-transparent items-center flex 
-                    justify-between gap-1 lg:gap-10 border-b-2 
-                    border-b-gray-300 sticky top-0 z-50 backdrop-blur-sm"
+            className=" sticky top-0 z-50 backdrop-blur-sm border-b-2 border-b-gray-300 bg-white/70"
         >
-            <div className="flex items-center lg:gap-4 gap-2">
-                <Link to={'/'}>
-                    <img src="/kitchen-logo.svg" height={100} width={100} />
-                </Link>
-                <h1 className="uppercase font-extralight lg:text-3xl 
-                    text-lg lg:motion-preset-typewriter-[15] lg:motion-duration-[8s] lg:block hidden"
-                >
-                    Kitchen Recipes
-                </h1>
-            </div>
-            <div className="flex lg:gap-10 gap-3 print:hidden">
-                {protectedLinks.includes(location.pathname)? 
-                    <Link 
-                        to={'/'} 
+            <div className="flex justify-between items-center p-4 lg:px-10">
+
+                {/* Logo link image */}
+                <div className="flex items-center gap-3">
+                    <Link to={'/'}>
+                        <img src="/kitchen-logo.svg" height={100} width={100} />
+                    </Link>
+                    <h1 className="uppercase font-extralight lg:text-3xl 
+                        text-lg lg:motion-preset-typewriter-[15] lg:motion-duration-[8s] lg:block hidden"
+                    >
+                        Kitchen Recipes
+                    </h1>
+                </div>
+                {/* Desktop menu */}
+                <div className="hidden lg:flex gap-6 items-center">
+                    {protectedLinks.includes(location.pathname) ?
+                        <Link
+                            to={'/'}
+                            className="text-right text-xs lg:text-sm border-b-black 
+                                border-b-2 uppercase font-extralight flex flex-row gap-2 p-2"
+                            onClick={logout}
+                        >
+                            <ArrowLeftStartOnRectangleIcon width={20} />
+                            Logout
+                        </Link> :
+                        <Link
+                            to={'/auth/login'}
+                            className="text-right text-xs lg:text-sm border-b-black 
+                                border-b-2 uppercase font-extralight flex flex-row p-2 gap-2"
+                        >
+                            <ArrowTopRightOnSquareIcon width={20} />
+                            Login
+                        </Link>
+                    }
+                    {!protectedLinks.includes(location.pathname) &&
+                        <Link
+                            to={'/auth/register'}
+                            className="text-right text-xs lg:text-sm border-b-black 
+                            border-b-2 uppercase font-extralight flex flex-row p-2 gap-2"
+                        >
+                            <ArrowRightEndOnRectangleIcon width={20} />
+                            Register
+                        </Link>}
+                    <Link
+                        to={'/recipes'}
                         className="text-right text-xs lg:text-sm border-b-black 
                             border-b-2 uppercase font-extralight flex flex-row gap-2 p-2"
-                        onClick={logout}
                     >
-                        <ArrowLeftStartOnRectangleIcon width={20}/>
-                        Logout
-                    </Link>:
-                    <Link
-                        to={'/auth/login'}
-                        className="text-right text-xs lg:text-sm border-b-black 
-                            border-b-2 uppercase font-extralight flex flex-row p-2 gap-2"
-                    >
-                        <ArrowTopRightOnSquareIcon width={20}/>
-                        Login
+                        <BookOpenIcon width={20} />
+                        Recipes
                     </Link>
-                }
-                {protectedLinks.includes(location.pathname) ? '' : <Link
-                    to={'/auth/register'}
-                    className="text-right text-xs lg:text-sm border-b-black 
-                        border-b-2 uppercase font-extralight flex flex-row p-2 gap-2"
+                    <Link
+                        to={'/recipes/book'}
+                        className="text-right text-xs lg:text-sm border-b-black
+                            border-b-2 uppercase font-extralight flex flex-row gap-2 p-2"
+                    >
+                        <BookmarkIcon width={20} />
+                        Saved
+                    </Link>
+                </div>
+                {/* Mobile button */}
+                <button
+                    className="lg:hidden"
+                    onClick={() => setIsOpen(!isOpen)}
                 >
-                    <ArrowRightEndOnRectangleIcon width={20}/>
-                    Register
-                </Link>}
-                <Link
-                    to={'/recipes'}
-                    className="text-right text-xs lg:text-sm border-b-black 
-                        border-b-2 uppercase font-extralight flex flex-row gap-2 p-2"
-                >
-                    <BookOpenIcon width={20}/>
-                    Recipes
-                </Link>
-                <Link 
-                    to={'/recipes/book'}
-                    className="text-right text-xs lg:text-sm border-b-black
-                        border-b-2 uppercase font-extralight flex flex-row gap-2 p-2"
-                >
-                    <BookmarkIcon width={20}/>
-                    Saved
-                </Link>
+
+                    {isOpen ? (
+                        <XMarkIcon width={30} />
+                    ) : (
+                        <Bars3Icon width={30} />
+                    )}
+
+                </button>
             </div>
+            {/* Mobile menu */}
+            {isOpen && (
+                <div className="flex flex-col gap-4 p-4 lg:hidden border-t border-gray-200 bg-white">
+
+                    {protectedLinks.includes(location.pathname) ? (
+
+                        <Link
+                            to={'/'}
+                            onClick={logout}
+                            className="mobile-nav-link"
+                        >
+                            <ArrowLeftStartOnRectangleIcon width={20} />
+                            Logout
+                        </Link>
+
+                    ) : (
+
+                        <Link
+                            to={'/auth/login'}
+                            className="mobile-nav-link"
+                        >
+                            <ArrowTopRightOnSquareIcon width={20} />
+                            Login
+                        </Link>
+
+                    )}
+
+                    {!protectedLinks.includes(location.pathname) && (
+
+                        <Link
+                            to={'/auth/register'}
+                            className="mobile-nav-link"
+                        >
+                            <ArrowRightEndOnRectangleIcon width={20} />
+                            Register
+                        </Link>
+
+                    )}
+
+                    <Link
+                        to={'/recipes'}
+                        className="mobile-nav-link"
+                    >
+                        <BookOpenIcon width={20} />
+                        Recipes
+                    </Link>
+
+                    <Link
+                        to={'/recipes/book'}
+                        className="mobile-nav-link"
+                    >
+                        <BookmarkIcon width={20} />
+                        Saved
+                    </Link>
+
+                </div>
+            )}
         </nav>
 
     )
