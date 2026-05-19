1bb2208f533546a2b60971d076c7ad45d032dab1
AlejandroMtz07 <alejandro23juanito@gmail.com>
2026-05-18 20:25:26 -0700

    Changing the size of the image and the height of the text part.

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -8,11 +8,11 @@ export default function HomeCard({text, image_url, reverse}:HomeCardProps) {
   return (
     <div 
         className={`flex ${reverse ? "lg:flex-row-reverse" : "lg:flex-row"} 
-            flex-col items-center align-middle lg:h-80 gap-10`}
+            flex-col items-center align-middle lg:h-full gap-10`}
     >
-        <img src={`${image_url}`} alt="Representative image" className="lg:w-full w-auto lg:h-full h-auto"/>
+        <img src={`${image_url}`} alt="Representative image" className="lg:w-1/2 w-auto lg:h-4/5 h-auto"/>
         <div 
-            className="p-4 lg:m-10 border-gray-200 flex items-center rounded-lg 
+            className="p-4 lg:m-10 border-gray-200 flex items-center rounded-lg h-1/2
             hover:shadow-xl border-2 transition text-center leading-loose font-thin"
         >
             {text}
