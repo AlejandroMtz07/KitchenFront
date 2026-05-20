fffd3e803a47d1677937594a4bfcab7166e53288
AlejandroMtz07 <alejandro23juanito@gmail.com>
2026-05-19 17:35:29 -0700

    Adapting the style of the pdf.

--- src/components/PDF.tsx
+++ src/components/PDF.tsx
@@ -1,9 +1,54 @@
-import { Document, Page, Text, StyleSheet, Link, Image } from '@react-pdf/renderer'
+import { Document, Page, Text, StyleSheet, Link, Image, View } from '@react-pdf/renderer'
 import type { PublicRecipe } from '../types'
 
 const styles = StyleSheet.create({
-	page: {
-		
+	title: {
+		padding: '10px',
+		height: '100px',
+		display: 'flex',
+		flexDirection: 'row',
+		alignItems: 'center',
+		backgroundColor: '#eeeeee',
+		justifyContent: 'center'
+	},
+	logo: {
+		width: '100px',
+		height: '100px'
+	},
+
+	recipes_container: {
+
+		display: 'flex',
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		justifyContent: 'space-between'
+
+	},
+
+	card: {
+
+		width: '48%',
+		border: '1 solid #d1d5db',
+		borderRadius: 6,
+		padding: 10,
+		marginBottom: 15
+
+	},
+
+	container_image: {
+
+		width: '100%',
+		height: 120,
+		objectFit: 'cover',
+		marginBottom: 10
+
+	},
+
+	recipe_title: {
+
+		fontSize: 14,
+		textAlign: 'center'
+
 	}
 })
 
@@ -11,18 +56,32 @@ type PDFProps = {
 	recipes: PublicRecipe[]
 }
 
-export default function PDF({recipes}:PDFProps) {
+export default function PDF({ recipes }: PDFProps) {
 	return (
 		<Document>
-			<Page size={'A4'}>
-				{/* Recipe renderer */}
-					{recipes.map(recipe =>(
-						<Text key={recipe.id}>
-							<Text>
+			<Page size="A4">
+				{/* Title of the recibe book */}
+				<View style={styles.title}>
+					<Image src={'../kitchen-logo.svg'} style={styles.logo} />
+					<Text>
+						Kitchen Recipes.
+					</Text>
+				</View>
+				<View style={styles.recipes_container}>
+
+					{/* Recipe card generate */}
+					{recipes.map(recipe => (
+						<View key={recipe.id}	style={styles.card}>
+							<Image
+								src={recipe.image}
+								style={styles.container_image}
+							/>
+							<Text style={styles.recipe_title}>
 								{recipe.name}
 							</Text>
-						</Text>
+						</View>
 					))}
+				</View>
 			</Page>
 		</Document>
 	)
