e071b2326a61558b9fb71c2591956d40d9783619
AlejandroMtz07 <alejandro23juanito@gmail.com>
2026-06-18 21:49:10 -0700

    Doing the modal container more responsive.

--- src/components/LoadingModal.tsx
+++ src/components/LoadingModal.tsx
@@ -9,8 +9,8 @@ export default function LoadingModal({ isLoading, message} : LoadingModalProps) 
     if(!isLoading) return null;
     return (
         
-        <div className="fixed top-1/4 lg:left-[38%] md:left-[30%] left-[7%] w-[50vh] h-[50vh] 
-        flex justify-center items-center z-[9999] bg-gray-700 bg-opacity-50 rounded-lg">
+        <div className="fixed top-1/4 lg:left-[38%] md:left-[30%] left-[5%] lg:w-[50vh] md:w-[50vh] w-[43vh] h-[50vh] 
+        flex justify-center items-center bg-gray-700 bg-opacity-50 rounded-lg">
             <div className="flex items-center justify-center flex-col">
                 <div className="rounded-full animate-spin">
                     <img src={"/images/cubiertos.png"} alt="Loading" className="h-20 w-20"/>
